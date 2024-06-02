6a3d490aa420b45efc46c54f3b7105069c5a925d
Jack Clark <jackclarkfal@gmail.com>
2024-06-02 15:07:31 +0100

    Made new script to convert PubMLST export sequences multifasta to fastas. Moved old script

--- Scripts/Conversions and Translations/multifasta_to_fasta.R
+++ Scripts/Conversions and Translations/multifasta_to_fasta.R
@@ -1,38 +1,34 @@
-###Script to separate multifasta files into individual sequence files
-setwd("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data")
-input <- read.fasta("IGR_for_BLAST_seqs.fa", as.string=TRUE, whole.header=TRUE)
+#This script converts the .txt output from PubMLST 'export sequences' from a multifasta to one fasta file per sequence.
+#Filenames are taken from the header of each fasta sequence
 
-#First thing is to separate sequences and locus details
-sequences <- c()
-loci <- c()
-for(i in 1:length(input)){
-  sequences <- c(sequences, input[[i]][1])
-  loci <- c(loci, names(input[i]))
+#Load packages
+library(seqinr)
+library(stringr)
+library(magrittr)
+
+main <- function(multifasta, outdir){
+  names(multifasta) %<>% truncate_ids()
+  for(i in 1:length(multifasta)){
+    sequence <- multifasta[i]
+    write.fasta(sequences = as.character(multifasta[i]), names = names(multifasta[i]),
+                file.out=paste(outdir, '/',  names(sequence), '.fasta', sep=''))
+  }
+  
 }
 
-#Add a '>' at the start of each locus name
-for(i in 1:length(loci)){
-  loci[i] <- paste('>', loci[i], sep = '')
+#Function to extract the isolate id and locus tag from the names of each sequence
+truncate_ids <- function(seq_names){
+  out <- c()
+  pattern = paste('(?<=', str_escape('+'), '[:space:])[:graph:]+', sep = '')
+  for(name in seq_names){
+    locustag <- str_extract(name, pattern)
+    isolate <- substr(name, start = 1, stop = 5)
+    newname <- paste(isolate, locustag, sep = '_')
+    out <- c(out, newname)
+  }
+  return(out)
 }
 
-#Remove the = signs at the end of every 8 entry
-for(i in 1:length(Coding)){
-  Coding[[i]][1] <- str_replace_all(Coding[[i]][1], '=', '')
-  IGR[[i]][1] <- str_replace_all(IGR[[i]][1], '=', '')
-}
-
-#Write each sequence locus pair into its own file
-setwd("BLAST_sequences")
-for(i in 1:length(loci)){
-
-  name_start <- gsub(".*\\s", "", loci[i])
-  name_end <- gsub(":.*", "", loci[i])
-  name_end <- substr(name_end, 2, nchar(name_end))
-  name <- paste(name_start, name_end, sep = '_')
-  filename <- paste(name, '.fasta', sep = '')
-  file <- c(loci[i], sequences[i])
-  #
-  write(file, filename)
-}
-
-substr(gsub(":.*", "", loci[i]), 2, nchar(gsub(":.", "", loci[i])))
+main(multifasta = read.fasta('~/Documents/PhD/PhD/PhasomeIt_data/PV_loci/PV_fastas.txt',
+                             as.string = TRUE, whole.header = TRUE),
+     outdir = '~/Documents/PhD/PhD/PhasomeIt_data/PV_loci/PV_fastas')
