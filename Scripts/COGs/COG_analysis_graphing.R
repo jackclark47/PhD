9dd8055562808286027ca7b2292501404fc31d6c
Jack Clark <jackclarkfal@gmail.com>
2024-11-05 15:20:35 +0000

    Updated file paths and file inputs. Included analysis of var vs novar

--- Scripts/COGs/COG_analysis_graphing.R
+++ Scripts/COGs/COG_analysis_graphing.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(gplots)
 library(corrplot)
 
-setwd("~/Documents/PhD/PhD/RNA_IGR/eggNOG_annotations/Isolates")
+setwd("~/Documents/PhD/RNA_IGR/eggNOG_annotations/Isolates")
 
 COGsum <- function(isolate, append){
   data <- read.xlsx(paste(isolate, '.annotations.xlsx', sep = ''), startRow = 3)[,7]
@@ -23,7 +23,7 @@ i8 <- COGsum('53951', TRUE)
 
 df <- as.data.frame(cbind(i1,i2,i3,i4,i5,i6,i7,i8))
 colnames(df) <- c('27509', '27553', '28262', '28287', '53930', '53948', '53951')
-
+setwd("~/Documents/PhD/RNA_IGR/eggNOG_annotations/")
 # write.xlsx(df, "eggNOG_COG_summaries_new.xlsx")
 
 
@@ -34,33 +34,44 @@ colnames(df) <- c('27509', '27553', '28262', '28287', '53930', '53948', '53951')
 
 #Section to compare the all_loci annotations to those in the rna expression dataset, and laater to the igr dataset
 #load rna loci
-rna <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/11-7_run_5_transcripts-cdb-16082022.xlsx", sheet=9)
+rna <- read.xlsx("~/Documents/PhD/RNA_IGR/11-7_run_5_transcripts-cdb-04092024.xlsx", sheet=8)
+rnanonsig <- rna[(which(as.numeric(rna$X13) < 0.05 | rna$`log2(EXP/Min)` < 1)), ]
+rna_underlog2 <- rna[which(rna$`log2(EXP/Min)` < 1),]
 rna <- rna[which(rna$`log2(EXP/Min)` >=1),]
 rnalog4 <- rna[which(rna$`log2(EXP/Min)` >=2),]
+rnasig <- rna[which(as.numeric(rna$X13) < 0.05),]
+
 #load distributions of cogs in all loci
-cogs <- read.xlsx('~/Documents/PhD/PhD/RNA_IGR/eggNOG_annotations/all_coding_loci.annotations.xlsx', startRow = 3)[1:1765,]
+cogs <- read.xlsx('~/Documents/PhD/RNA_IGR/eggNOG_annotations/all_coding_loci.annotations.xlsx', startRow = 3)[1:1765,]
 
 #match loci in the two lists using log2fold change >1 and >2 as cutoffs
+
 cogslog2 <- cogs[which(cogs$query %in% rna$`1717.genes`),] 
-length(unique(rna$`1717.genes`)) #590 loci, of which 404 have cog information
+length(unique(rna$`1717.genes`)) #780 loci, of which 546 have cog information
 cogslog4 <- cogs[which(cogs$query %in% rnalog4$`1717.genes`),] 
-length(unique(rnalog4$`1717.genes`)) #105 loci, of which 76 have cog information
-cogsunderlog2 <- cogs[which(!(cogs$query %in% rna$`1717.genes`)),]
-cogsunderlog2 <- cogsunderlog2[which(!(cogsunderlog2$query %in% rnalog4$`1717.genes`)),]
+length(unique(rnalog4$`1717.genes`)) #165 loci, of which 115 have cog information
+cogsunderlog2 <- cogs[which(cogs$query %in% rna_underlog2$`1717.genes`),] 
+
+cogssig <- cogs[which(cogs$query %in% rnasig$`1717.genes`),] 
+cogsnonsig <- cogs[which(cogs$query %in% rnanonsig$`1717.genes`),] 
 
 #get table of COG categories for those two cutoffs
 dataset <- list('log2>1' = table(cogslog2$COG_category), 'log2>2' = table(cogslog4$COG_category), 'under_log2' = table(cogsunderlog2$COG_category))
-# write.xlsx(dataset, file = 'rna_cogstest.xlsx')
+#write.xlsx(dataset, file = 'rna_cogstest.xlsx')
+
+dataset <- list('sig' = table(cogssig$COG_category), 'nonsig' = table(cogsnonsig$COG_category))
+#write.xlsx(dataset, file = 'sig_rna_cogs.xlsx')
+
 
 
 #now for igr categories
 #load lists of loci present in each category
-upvar <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 6)
-downvar <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 5)
-highvar <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 7)
-lowvar <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 4)
-novar <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 2)
-var <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/Isolate_Igr_Data/igr_new_11012024.xlsx", sheet = 3)
+upvar <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 7)
+downvar <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 4)
+highvar <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 5)
+lowvar <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 6)
+novar <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 2)
+var <- read.xlsx("~/Documents/PhD/RNA_IGR/Isolate_Igr_Data/Correct/igrs_all.xlsx", sheet = 3)
 #what are the cog distributions in each category?
 upvarcogs <- cogs[which(cogs$query %in% upvar$Locus),][,c(1,7)]
 downvarcogs <- cogs[which(cogs$query %in% downvar$Locus),][,c(1,7)]
@@ -90,16 +101,20 @@ categories <- c("Unassigned", "C: Energy production/conversion", "D: Cell cycle,
                 "U: Intracellular trafficking, secretion, vesicular transport", "V: Defense mechanisms")
 
 #for rna data
-rna_counts <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/eggNOG_annotations/rna_cogs.xlsx")[1:22, c(3,4,6,9)]
+rna_counts <- read.xlsx("~/Documents/PhD/RNA_IGR/eggNOG_annotations/rna_cogstest.xlsx")[1:22,]
 colnames(rna_counts) <- c('COG_category', '2fold', '4fold', 'under_2fold')
 rownames(rna_counts) <- rna_counts$COG_category
 rna_counts <- rna_counts[,-1]
 #for igr data
-igr_counts <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/eggNOG_annotations/igr_cogs.xlsx")[1:22,-c(1,2, 10:16)]
+igr_counts <- read.xlsx("~/Documents/PhD/RNA_IGR/eggNOG_annotations/igr_cogs.xlsx")[1:22,c(3:9)]
 colnames(igr_counts) <- c('COG_category', 'upvar', 'downvar', 'highvar', 'lowvar', 'novar', 'var')
 rownames(igr_counts) <- igr_counts$COG_category
 igr_counts <- igr_counts[,-1]
 
+sig_counts <- read.xlsx("~/Documents/PhD/RNA_IGR/eggNOG_annotations/sig_rna_cogs.xlsx")[1:20,c(1:3)]
+colnames(sig_counts) <- c('COG_category', 'sig', 'nonsig')
+rownames(sig_counts) <- sig_counts$COG_category
+sig_counts <- sig_counts[,-1]
 #need to exclude data where the expected is less than 5 counts
 igr_counts <- igr_counts[-c(2,22),]
 rna_counts <- rna_counts[-c(2,22),]
@@ -110,20 +125,39 @@ categories <- c("Unassigned", "C: Energy production/conversion", "D: Cell cycle,
                 "U: Intracellular trafficking, secretion, vesicular transport", "V: Defense mechanisms")
 rownames(igr_counts) <- categories
 rownames(rna_counts) <- categories
+rownames(sig_counts) <- categories
+
 #compute chisquared tests
 rna_chisq <- chisq.test(rna_counts)
 rna_chisq$p.value #significant between 2 fold and below 2 fold. barely not significant between all 3
-corrplot(rna_chisq$residuals, method = 'color', is.cor = FALSE, tl.cex = 1, tl.col = 'black', 
+corrplot(rna_chisq$residuals, method = 'color', is.cor = FALSE, tl.cex = 0.9, tl.col = 'black', 
          cl.ratio = 0.5,
          cl.align.text = 'l',
          cl.offset = 0.2)
 
-igr_chisq <- chisq.test(igr_counts[-c(11),c(1:5)])
+igr_chisq <- chisq.test(igr_counts[,c(1:5)])
 igr_chisq$p.value
 corrplot(igr_chisq$residuals, method = 'color',is.cor = FALSE, tl.cex = 1, tl.col = 'black',
          cl.ratio = 0.3,
          cl.align.text = 'l',
          cl.offset = 0.2)
+
+
+var_chisq <- chisq.test(igr_counts[,c(5:6)])
+var_chisq$p.value
+corrplot(var_chisq$residuals, method = 'color',is.cor = FALSE, tl.cex = 1, tl.col = 'black',
+         cl.ratio = 0.8,
+         cl.align.text = 'l',
+         cl.offset = 0.2)
+
+
+sig_chisq <- chisq.test(sig_counts)
+sig_chisq$p.value
+corrplot(sig_chisq$residuals, method = 'color',is.cor = FALSE, tl.cex = 1, tl.col = 'black',
+         cl.ratio = 0.8,
+         cl.align.text = 'l',
+         cl.offset = 0.2)
+
 
 rna_chisq$observed
 round(rna_chisq$expected,2)
@@ -131,6 +165,7 @@ round(rna_chisq$expected,2)
 contrib_rna <- 100*rna_chisq$residuals^2/rna_chisq$statistic
 contrib_rna <- round(contrib_rna,3)
 corrplot(contrib_rna, is.corr = F)
+
 
 contrib_igr <- 100*igr_chisq$residuals^2/igr_chisq$statistic
 contrib_igr <- round(contrib_igr,3)
@@ -144,7 +179,7 @@ corrplot(contrib_igr, is.corr = F)
 #Repeat for each isolate to see if there are differences between them
 
 #load isolate spreadsheet
-isolate_cogs <- read.xlsx("~/Documents/PhD/PhD/RNA_IGR/eggNOG_annotations/eggNOG_COG_summaries.xlsx")[1:22,9:16]
+isolate_cogs <- read.xlsx("~/Documents/PhD/RNA_IGR/eggNOG_annotations/Old/eggNOG_COG_summaries.xlsx")[1:22,9:16]
 isolate_cogs <- isolate_cogs[-c(2,22),]
 rownames(isolate_cogs) <- categories
 isolate_cogs <- isolate_cogs[,-c(1)]
