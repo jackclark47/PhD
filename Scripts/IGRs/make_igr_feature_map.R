38d69f33d4668aca7fe088335d11476f68c3da9d
Jack Clark <jackclarkfal@gmail.com>
2024-11-05 15:11:28 +0000

    Initial commit

---
+++ Scripts/IGRs/make_igr_feature_map.R
@@ -0,0 +1,20 @@
+#Make a feature file of igrs based on N222.1.2 hybrid sequence ready to visualise on the N222.1.2 genome
+#name #start  #stop #var  
+
+#Load igr data
+upvar
+downvar
+lowvar
+highvar
+nowvar
+
+#load N222 genbank file
+
+
+#Annotate N222 with BLAST search against mini pubmlst database
+
+
+#write as genbank file
+
+
+#for each locus in N222 check which vargroup it belongs to and write that to a feature file
